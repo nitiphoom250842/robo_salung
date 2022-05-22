6a48cf8f8f751d3679ce96249e3f714690f07ee9
nitiphoom <nitiphoom.y@gmail.com>
2022-05-22 16:29:21 +0700

    add ai

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] ='3'
 from typing import Union
+import datetime
 from fastapi import FastAPI
 import cv2
 import tensorflow as tf
@@ -9,22 +10,45 @@ import json
 import numpy as np
 
 
-with open('./application/model_ai/class_name.json', 'r',encoding='utf8') as json_class:
+with open('./model_ai/class_name.json', 'r',encoding='utf8') as json_class:
     class_names = json.loads(json_class.read())
 
 
-with open('./application/model_ai/architecture.json', 'r') as json_file:
+with open('./model_ai/architecture.json', 'r') as json_file:
     model_j = model_from_json(json_file.read())
-    model_j.load_weights('./application/model_ai/model.h5')
+    model_j.load_weights('./model_ai/model.h5')
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"data": 1}
+    start_time=datetime.datetime.now()
+    data=ai()
+    end_time=datetime.datetime.now()
+    return {"result": data[0],
+            "score":data[1],
+            "time":end_time-start_time}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+
+def ai():
+    
+    # ret,img = cap.read()
+    img=cv2.imread('./images/2022-05-21 09_37_46.491272.jpg')
+    img_height=224
+    img_width=224
+    img_resize =cv2.resize(img,(img_height,img_width))
+    # cv2.imwrite('./image/test_.jpg',img)
+    img_resize =cv2.resize(img,(img_height,img_width))
+    img_array = tf.keras.utils.img_to_array(img_resize)
+    img_array = tf.expand_dims(img_array, 0) # Create a batch
+    predictions = model_j.predict(img_array)
+    score = tf.nn.softmax(predictions[0])
+    print(class_names[np.argmax(score)],"ค่าความแม่นยำที่",100 * np.max(score))
+    return class_names[np.argmax(score)],100 * np.max(score)
