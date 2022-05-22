394825d323be370e60dde7079641f85cf45ac372
nitiphoom <nitiphoom@looksocial.asia>
2022-05-22 12:27:30 +0700

    add ai

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] ='3'
 from typing import Union
-
 from fastapi import FastAPI
+import cv2
+import tensorflow as tf
+from tensorflow.keras.models import model_from_json
+import json
+import numpy as np
+
+
+with open('./application/model_ai/class_name.json', 'r',encoding='utf8') as json_class:
+    class_names = json.loads(json_class.read())
+
+
+with open('./application/model_ai/architecture.json', 'r') as json_file:
+    model_j = model_from_json(json_file.read())
+    model_j.load_weights('./application/model_ai/model.h5')
 
 app = FastAPI()
 
